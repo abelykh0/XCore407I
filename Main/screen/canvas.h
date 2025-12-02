b84c6d4c70a28419b170d45abe6e6a2c49f421bd
Andrey Belykh <abelykh@yahoo.ca>
2025-12-01 19:43:09 -0500

    Added comments

--- Main/screen/canvas.h
+++ Main/screen/canvas.h
@@ -9,13 +9,9 @@ extern "C" {
 #include "usbd_video_conf.h"
 
 // TODO
-// - support 256x192 as 2x (512x384 YUY2)
-// - STM32F407 only has 192K RAM, 2x YUY2 would not fit
+// - support 256x192 as 2x (512x384 NV12)
+// - STM32F407 only has 192K RAM, 512x384 NV12 would not fit in RAM
 // - store as 4 bits per pixel (64 indexed colors), 256x192 will need only 24 KB
-// - the UVC size is 2x
-// - when streaming expand each 4 bit pixel to 32 bits (YUY2)
-// - repeat row 2x
-// - maybe also support 4x (1024x768)
 
 extern uint8_t canvas_buffer[12];
 
